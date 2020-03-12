1f07885af1616a042021320eab3a16fa88321bad
Otávio Goes <otavio@ogoes.dev>
2020-03-12 20:57:37 -0300

    Faça um programa que inicie três threads e, cada thread, espere um tempo aleatório para terminar

--- 1random_time.cpp
+++ 1random_time.cpp
@@ -7,7 +7,7 @@
 /**
  * @author Otávio Goes
  * @date 12/03/2020
- * 
+ *
  * Faça um programa que inicie três threads e, cada thread, espere um tempo
  * aleatório para terminar.
  */
@@ -15,27 +15,23 @@
 int main() {
   srand(time(NULL));
 
-  std::thread thread1([]() {
-    int time_to_wait = rand() % 100;
-    std::this_thread::sleep_for(std::chrono::milliseconds(time_to_wait));
-    std::cout << "Thread 1 acabou: " << time_to_wait << "ms" << std::endl;
-  });
+  long int thread_number = 3;
 
-  std::thread thread2([]() {
-    int time_to_wait = rand() % 100;
-    std::this_thread::sleep_for(std::chrono::milliseconds(time_to_wait));
-    std::cout << "Thread 2 acabou: " << time_to_wait << "ms" << std::endl;
-  });
+  std::thread threads[thread_number];
 
-  std::thread thread3([]() {
-    int time_to_wait = rand() % 100;
-    std::this_thread::sleep_for(std::chrono::milliseconds(time_to_wait));
-    std::cout << "Thread 3 acabou: " << time_to_wait << "ms" << std::endl;
-  });
+  for (int i = 0; i < thread_number; ++i)
+    threads[i] = std::thread([i]() {
+      int time_to_wait = rand() % 100;
 
-  thread1.join();
-  thread2.join();
-  thread3.join();
+      std::this_thread::sleep_for(std::chrono::milliseconds(time_to_wait));
+
+      std::cout << "Thread " << i << " acabou: " << time_to_wait << "ms"
+                << std::endl;
+    });
+
+  for (auto &thread : threads) {
+    if (thread.joinable()) thread.join();
+  }
 
   return 0;
 }
